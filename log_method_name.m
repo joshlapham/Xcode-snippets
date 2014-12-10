6a2b8c23e94a033fbb52bf4d1009ab0fac9ff083
Josh Lapham <josh@inthecode.com.au>
2014-12-10 10:10:12 +1000

    Imported user code snippets

---
+++ log_method_name.m
@@ -0,0 +1,10 @@
+// Log method name
+// NSLog current class name and method name
+//
+// IDECodeSnippetCompletionPrefix: logmeth
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 09CB75C4-B422-43DC-B945-BCABBB971BC7
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+NSLog(@"%@ - %s", [self class], __func__);
